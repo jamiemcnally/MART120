ce1b26a41e29461e72ef49d6e47e39062f864a45
Jamie McNally <jamie.mcnally@umontana.edu>
2022-10-31 17:35:02 -0600

    Finished Homework 9

--- Homework 9/js/sketch.js
+++ Homework 9/js/sketch.js
@@ -5,9 +5,38 @@ function setup()
 
 function draw()
 {
-  background(120, 45, 78);
-  fill(255, 204, 0)
+  background(34, 139, 34);
+  // head
+  fill(232, 190, 172)
   circle(250, 100, 175);
+  // body
+  fill(255, 105, 180);
+  rect(200, 185, 100, 150);
+  // eyes
+  strokeWeight(10);
+  fill(0);
+  point(210, 75);
+  point(285, 75);
+  // nose
+  point(245, 100)
+  // mouth
+  circle(250, 135, 25);
+  // right arm
   fill(10, 24, 120);
-  rect(210, 185, 100, 150);
+  rect(300, 195, 100, 15);
+  // left arm
+  rect(100, 195, 100, 15);
+  // hair
+  line(130, 175, 175, 50);
+  line(325, 50, 360, 175);
+  fill(255);
+  triangle(220, 320, 250, 220, 280, 320);
+  // left leg
+  rect(200, 335, 15, 100);
+  // right leg
+  rect(285, 335, 15, 100);
+
+  fill(255, 105, 180);
+  textSize(24);
+  text("Jamie McNally", 270, 500);
 }
