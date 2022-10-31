23ee903583595b5b046f8c38324cf919a22591ac
Jamie McNally <jamie.mcnally@umontana.edu>
2022-10-31 16:31:04 -0600

    updated

--- Homework 9/js/sketch.js
+++ Homework 9/js/sketch.js
@@ -1,9 +1,13 @@
 function setup()
 {
-  createCanvas(500,600);
+  createCanvas(500, 600);
 }
 
 funtion draw()
 {
-  background(120,45,78);
+  background(120, 45, 78);
+  fill(255, 204, 0)
+  circle(250, 100, 175);
+
+  rect(250, 170, 100, 150);
 }
